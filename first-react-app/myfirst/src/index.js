2218b923b0446070ae8d066cc4b4a2dd421a1d88
jod35 <jodestrevin@gmail.com>
2020-06-12 23:43:30 -0400

    nav overlay

---
+++ first-react-app/myfirst/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// function Hello() {
+//   return <h1>Hello World</h1>;
+// }
+
+function Hello()
+ReactDOM.render(<Hello />, document.getElementById("root"));
